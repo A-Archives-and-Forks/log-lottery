340b0bf103fa8a86a5b5c02850bde88d5fad033f
LOG1997 <LOG1997@users.noreply.github.com>
2026-01-20 01:47:44 +0000

    deploy: af4d562a200b898a1e697b5f7835257f4a7a40e4

---
+++ js/chunk-CmgVMalc.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-KnS5xexs.js";import{Q as o,o as s,P as t}from"./chunk-DjcfpCfW.js";const n=r({},[["render",function(r,n){const a=t("router-view");return s(),o(a)}]]);export{n as default};
